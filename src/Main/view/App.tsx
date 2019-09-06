da2afe88d46be807082b9dc66ef3f86178d7b24b
Muhun Kim <iam@muhun.kim>
2019-09-06 23:05:07 +0900

    feat: build login and logout interface

--- src/Main/view/App.tsx
+++ src/Main/view/App.tsx
@@ -1,11 +1,15 @@
-import { h, FunctionComponent } from 'preact'
+import { h, FunctionComponent, Fragment } from 'preact'
+import Logout from './Logout.tsx'
 
 interface AppProps {
   optOutUrl: string
 }
 
 const App: FunctionComponent<AppProps> = ({ optOutUrl }) => (
-  <div>{optOutUrl && <a href={optOutUrl}>원본 페이지 보기</a>}</div>
+  <Fragment>
+    <Logout />
+    <div>{optOutUrl && <a href={optOutUrl}>원본 페이지 보기</a>}</div>
+  </Fragment>
 )
 
 export default App
