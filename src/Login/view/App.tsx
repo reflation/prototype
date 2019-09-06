b666d797a9f9d25db9bbda26ad86b442100f0e10
Muhun Kim <iam@muhun.kim>
2019-09-06 23:05:07 +0900

    feat: build login and logout interface
    I created the login and main routing structure and session verification.

--- src/Login/view/App.tsx
+++ src/Login/view/App.tsx
@@ -1,11 +1,70 @@
-import { h, FunctionComponent } from 'preact'
+import { h, FunctionComponent, Fragment } from 'preact'
+import { useState } from 'preact/hooks'
+
+import axios from 'axios'
+
+import base64 from '../../utils/base64'
 
 interface AppProps {
   optOutUrl: string
 }
 
-const App: FunctionComponent<AppProps> = ({ optOutUrl }) => (
-  <div>{optOutUrl && <a href={optOutUrl}>원본 페이지 보기</a>}</div>
-)
+const App: FunctionComponent<AppProps> = ({ optOutUrl }) => {
+  const [id, setId] = useState('')
+  const [pw, setPw] = useState('')
+
+  const handleSubmit = async (e: Event) => {
+    const id: string = e.target[0].value
+    const pw: string = e.target[1].value
+    const encForm = new FormData()
+    encForm.append('tmpu', base64(id))
+    encForm.append('tmpw', base64(pw))
+
+    e.preventDefault()
+
+    const { data } = await axios.post<string>(
+      'https://dreamy.jejunu.ac.kr/frame/sysUser.do?next=',
+      encForm,
+      { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
+    )
+
+    if (data.includes('dbError')) {
+      const invaildMessage = data
+        .match(/var dbError = "(?:\.|(\\\")|[^\""\n])*"/)[0]
+        .slice(15, -1)
+
+      alert(invaildMessage)
+      return
+    }
+    location.href = 'main.do'
+  }
+
+  return (
+    <Fragment>
+      <form onSubmit={handleSubmit}>
+        <label>
+          ID:
+          <input
+            type="text"
+            value={id}
+            onChange={e => setId(e.target.value)}
+            autocomplete="username"
+          />
+        </label>
+        <label>
+          PW:
+          <input
+            type="password"
+            value={pw}
+            onChange={e => setPw(e.target.value)}
+            autocomplete="current-password"
+          />
+        </label>
+        <input type="submit" value="로그인" />
+      </form>
+      <footer>{optOutUrl && <a href={optOutUrl}>원본 페이지 보기</a>}</footer>
+    </Fragment>
+  )
+}
 
 export default App
