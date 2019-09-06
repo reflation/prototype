2f3ba78b2d3d72c52ab00493ef37d2508dbfba41
Muhun Kim <iam@muhun.kim>
2019-09-06 11:54:02 +0900

    init: build product with preact
    hello world to browser extenstion + preact

---
+++ src/Main/index.ts
@@ -0,0 +1,15 @@
+import '../scss/reset.scss'
+import { reconstruct, isOptOut, optOutUrl } from '../utils/reconstruct.ts'
+
+import { h, render } from 'preact'
+if (process.env.NODE_ENV === 'development') require('preact/debug')
+
+import App from './view/App.tsx'
+
+async function main() {
+  if (isOptOut()) return
+  reconstruct('메인')
+  render(h(App, { optOutUrl: optOutUrl() }), document.body)
+}
+
+main().catch(e => console.error(e ? e.stack || e : e))
