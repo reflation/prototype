a693a6385d91a64ed8a3797139457a90e23366a8
Muhun Kim <iam@muhun.kim>
2019-09-08 21:08:34 +0900

    policy(hot): improvement code quality
    solves codacy warning

--- webpack/entry.config.ts
+++ webpack/entry.config.ts
@@ -1,10 +1,11 @@
-import getSharedConfig from './getSharedConfig'
+import { getSharedWebpackConfig } from './getSharedConfig.ts'
 import * as path from 'path'
 
-const config = getSharedConfig({
+const config = getSharedWebpackConfig({
   entry: { login: './src/Login/index.ts', main: './src/Main/index.ts' },
   output: {
     filename: '[name].js',
+    path: path.resolve(__dirname, '..', 'public'),
   },
 })
 
