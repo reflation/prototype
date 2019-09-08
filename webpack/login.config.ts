c3ffae77f543e1a9b8a51e141d1e9129116ae316
Muhun Kim <iam@muhun.kim>
2019-09-08 21:16:09 +0900

    file(remove): unuse webpack configs

--- webpack/login.config.ts
+++
@@ -1,9 +0,0 @@
-import getSharedConfig from './getSharedConfig'
-
-const config = getSharedConfig({
-  entry: './src/Login/index.ts',
-  filename: 'bundle.js',
-  outputDir: 'public/Login',
-})
-
-export default config
--- webpack/main.config.ts
+++
@@ -1,9 +0,0 @@
-import getSharedConfig from './getSharedConfig'
-
-const config = getSharedConfig({
-  entry: './src/Main/index.ts',
-  filename: 'bundle.js',
-  outputDir: 'public/Main',
-})
-
-export default config
